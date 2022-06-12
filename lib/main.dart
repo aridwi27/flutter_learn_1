ed67648840dc006deaa4d55091eafd5c74ae60d8
aridwi27 <sukmawanaridwi27@gmail.com>
2022-06-12 14:53:41 +0700

    seaction 2 number 34

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,36 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('this is the answer');
+  }
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('hallo world'),
-    );
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('hallo'),
+            ),
+            body: Column(
+              children: [
+                Text('hallow'),
+                RaisedButton(
+                  child: Text('anser 1'),
+                  onPressed: answerQuestion,
+                ),
+                RaisedButton(
+                  child: Text('anser 2'),
+                  onPressed: () => print('button 2'),
+                ),
+                RaisedButton(
+                  child: Text('anser 3'),
+                  onPressed: () {
+                    print('button 3');
+                    print('answer 3');
+                  },
+                ),
+              ],
+            )));
   }
 }
